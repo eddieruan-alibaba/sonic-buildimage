6e574ed192203ab0b9efe2efc96d8a1fd65c8c4b
Eddie Ruan <eddie.ruan@alibaba-inc.com>
2024-10-28 20:31:51 +0800

    Add a compile flag to enable sonic-fpm

--- dockers/docker-fpm-frr/zebra.sh
+++ dockers/docker-fpm-frr/zebra.sh
@@ -2,9 +2,7 @@
 
 cmd="/usr/lib/frr/zebra -A 127.0.0.1 -s 90000000 -M dplane_fpm_nl -M snmp"
 
-hwsku=$(redis-cli -n 4  hget "DEVICE_METADATA|localhost" "hwsku")
-
-if [ $hwsku = "cisco-8101-p4-32x100-vs" ]; then
+if [ -f /etc/sonic/enable_sonic_fpm ]; then
     cmd="/usr/lib/frr/zebra -A 127.0.0.1 -s 90000000 -M dplane_fpm_sonic -M snmp"
 fi
 
