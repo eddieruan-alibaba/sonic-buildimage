7067db4de11ec567418a6bf2b9a5f7711d405696
Eddie Ruan <eddie.ruan@alibaba-inc.com>
2025-09-04 11:12:09 -0700

    Update PIC_CONTEXT to PIC_NH

--- src/sonic-frr/dplane_fpm_sonic/dplane_fpm_sonic.c
+++ src/sonic-frr/dplane_fpm_sonic/dplane_fpm_sonic.c
@@ -2413,8 +2413,8 @@ static int fpm_nl_enqueue(struct fpm_nl_ctx *fnc, struct zebra_dplane_ctx *ctx)
 	if ((!fnc->use_nhg)
 	    && (op == DPLANE_OP_NH_DELETE || op == DPLANE_OP_NH_INSTALL
 		|| op == DPLANE_OP_NH_UPDATE
-		|| op == DPLANE_OP_PIC_CONTEXT_DELETE || op == DPLANE_OP_PIC_CONTEXT_INSTALL
-		|| op == DPLANE_OP_PIC_CONTEXT_UPDATE))
+		|| op == DPLANE_OP_PIC_NH_DELETE || op == DPLANE_OP_PIC_NH_INSTALL
+		|| op == DPLANE_OP_PIC_NH_UPDATE))
 			return 0;
 
 	/*
@@ -2548,7 +2548,7 @@ static int fpm_nl_enqueue(struct fpm_nl_ctx *fnc, struct zebra_dplane_ctx *ctx)
 		nl_buf_len += (size_t)rv;
 		break;
 
-	case DPLANE_OP_PIC_CONTEXT_DELETE:
+	case DPLANE_OP_PIC_NH_DELETE:
 		rv = netlink_pic_context_msg_encode(RTM_DELNEXTHOP, ctx, nl_buf,
 						sizeof(nl_buf));
 		if (rv <= 0) {
@@ -2559,8 +2559,8 @@ static int fpm_nl_enqueue(struct fpm_nl_ctx *fnc, struct zebra_dplane_ctx *ctx)
 
 		nl_buf_len = (size_t)rv;
 		break;
-	case DPLANE_OP_PIC_CONTEXT_INSTALL:
-	case DPLANE_OP_PIC_CONTEXT_UPDATE:
+	case DPLANE_OP_PIC_NH_INSTALL:
+	case DPLANE_OP_PIC_NH_UPDATE:
 		rv = netlink_pic_context_msg_encode(RTM_NEWNEXTHOP, ctx, nl_buf,
 						sizeof(nl_buf));
 		if (rv <= 0) {
