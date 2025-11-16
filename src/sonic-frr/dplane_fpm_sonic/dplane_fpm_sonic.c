19ac19cb20710a921ca4d43d5a04819adae062e1
Eddie Ruan <eddie.ruan@alibaba-inc.com>
2025-11-16 11:02:49 +0800

    update header file include location

--- src/sonic-frr/dplane_fpm_sonic/dplane_fpm_sonic.c
+++ src/sonic-frr/dplane_fpm_sonic/dplane_fpm_sonic.c
@@ -57,7 +57,7 @@
 #include "zebra/zebra_srv6.h"
 #include "fpm/fpm.h"
 #include "lib/srv6.h"
-#include "common/c-api/nexthopgroup_capi.h"
+#include <swss/c-api/nexthopgroup_capi.h>
 
 #define SOUTHBOUND_DEFAULT_ADDR INADDR_LOOPBACK
 #define SOUTHBOUND_DEFAULT_PORT 2620
