4d3c55e187ed0342fd322c4c0d9eb4c0de156e13
Eddie Ruan <eddie.ruan@alibaba-inc.com>
2026-02-07 07:37:44 -0800

    Change msg

--- src/sonic-frr/dplane_fpm_sonic/dplane_fpm_sonic.c
+++ src/sonic-frr/dplane_fpm_sonic/dplane_fpm_sonic.c
@@ -3497,10 +3497,8 @@ void fib_init_logging(void) {
     /* Register callback BEFORE any fib_LOG() calls */
     fib_frr_register_callback(frr_log_forwarder);
     fib_frr_set_log_level(0);  // DEBUG
-	frr_log_forwarder(0, 
-		__FILE__, __LINE__, __func__, 
-		"FIB logging initialized and forwarding to FRR, log level set to %d",
-		fib_frr_get_log_level());
+	zlog_err("%s : FIB logging initialized and forwarding to FRR, log level set to %d",
+		__func__, fib_frr_get_log_level());
 }
 
 static int fpm_nl_init(void)
